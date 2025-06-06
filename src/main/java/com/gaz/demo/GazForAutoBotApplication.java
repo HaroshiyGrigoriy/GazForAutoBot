df6bbc6aa6ab355066ac60806fd395abcd636b8c
HaroshiyGrigoriy <guruwolf8@yandex.ru>
2025-06-06 16:10:18 +0300

    Create app

---
+++ src/main/java/com/gaz/demo/GazForAutoBotApplication.java
@@ -0,0 +1,13 @@
+package com.gaz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GazForAutoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GazForAutoBotApplication.class, args);
+	}
+
+}
